69d8169cd54f9af78c8c12fbf92e060e9cbef8a4
guillu97 <spore.gui@hotmail.fr>
2018-02-14 21:33:18 +0100

    added to do things:  to implement the http request with volley and bypass the nginx module

--- app/src/main/java/a1pour1/hebergratuit/net/a1pour1/JSONParser.java
+++ app/src/main/java/a1pour1/hebergratuit/net/a1pour1/JSONParser.java
@@ -27,6 +27,9 @@ import org.json.JSONObject;
 
 import android.util.Log;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
 public class JSONParser {
 
     static InputStream is = null;
@@ -151,4 +154,188 @@ public class JSONParser {
         return jObj;
 
     }
+
+    // tests for volley module
+    // TOD DO : https://www.androidtutorialpoint.com/networking/android-volley-tutorial/
+    // and this to bypass the nginx module   https://stackoverflow.com/questions/40806052/generating-cookie-from-website-that-implements-testcookie-nginx-module
+
+
+    /*public void volleyStringRequst(String url){
+
+        String  REQUEST_TAG = "com.androidtutorialpoint.volleyStringRequest";
+        //progressDialog.setMessage("Loading...");
+        ///progressDialog.show();
+
+        StringRequest strReq = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response.toString());
+
+                LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                showDialogView = li.inflate(R.layout.show_dialog, null);
+                outputTextView = (TextView)showDialogView.findViewById(R.id.text_view_dialog);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBuilder.setView(showDialogView);
+                alertDialogBuilder
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        })
+                        .setCancelable(false)
+                        .create();
+                outputTextView.setText(response.toString());
+                alertDialogBuilder.show();
+                progressDialog.hide();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                progressDialog.hide();
+            }
+        });
+        // Adding String request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq, REQUEST_TAG);
+    }
+
+    public void volleyJsonObjectRequest(String url){
+
+        String  REQUEST_TAG = "com.androidtutorialpoint.volleyJsonObjectRequest";
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        JsonObjectRequest jsonObjectReq = new JsonObjectRequest(url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+
+                        LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                        showDialogView = li.inflate(R.layout.show_dialog, null);
+                        outputTextView = (TextView)showDialogView.findViewById(R.id.text_view_dialog);
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                        alertDialogBuilder.setView(showDialogView);
+                        alertDialogBuilder
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                    }
+                                })
+                                .setCancelable(false)
+                                .create();
+                        outputTextView.setText(response.toString());
+                        alertDialogBuilder.show();
+                        progressDialog.hide();
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                progressDialog.hide();
+            }
+        });
+
+        // Adding JsonObject request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectReq,REQUEST_TAG);
+    }
+    public void volleyJsonArrayRequest(String url){
+
+        String  REQUEST_TAG = "com.androidtutorialpoint.volleyJsonArrayRequest";
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        JsonArrayRequest jsonArrayReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                        showDialogView = li.inflate(R.layout.show_dialog, null);
+                        outputTextView = (TextView)showDialogView.findViewById(R.id.text_view_dialog);
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                        alertDialogBuilder.setView(showDialogView);
+                        alertDialogBuilder
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                    }
+                                })
+                                .setCancelable(false)
+                                .create();
+                        outputTextView.setText(response.toString());
+                        alertDialogBuilder.show();
+                        progressDialog.hide();                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                progressDialog.hide();
+            }
+        });
+
+        // Adding JsonObject request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayReq, REQUEST_TAG);
+    }
+
+    public void volleyImageLoader(String url){
+        ImageLoader imageLoader = AppSingleton.getInstance(getApplicationContext()).getImageLoader();
+
+        imageLoader.get(url, new ImageLoader.ImageListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Image Load Error: " + error.getMessage());
+            }
+
+            @Override
+            public void onResponse(ImageLoader.ImageContainer response, boolean arg1) {
+                if (response.getBitmap() != null) {
+
+                    LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                    showDialogView = li.inflate(R.layout.show_dialog, null);
+                    outputImageView = (ImageView)showDialogView.findViewById(R.id.image_view_dialog);
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                    alertDialogBuilder.setView(showDialogView);
+                    alertDialogBuilder
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                }
+                            })
+                            .setCancelable(false)
+                            .create();
+                    outputImageView.setImageBitmap(response.getBitmap());
+                    alertDialogBuilder.show();
+                }
+            }
+        });
+    }
+
+    public void volleyCacheRequest(String url){
+        Cache cache = AppSingleton.getInstance(getApplicationContext()).getRequestQueue().getCache();
+        Cache.Entry entry = cache.get(url);
+        if(entry != null){
+            try {
+                String data = new String(entry.data, "UTF-8");
+                // handle data, like converting it to xml, json, bitmap etc.,
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+
+        }
+    }
+
+    public void volleyInvalidateCache(String url){
+        AppSingleton.getInstance(getApplicationContext()).getRequestQueue().getCache().invalidate(url, true);
+    }
+
+    public void volleyDeleteCache(String url){
+        AppSingleton.getInstance(getApplicationContext()).getRequestQueue().getCache().remove(url);
+    }
+
+    public void volleyClearCache(){
+        AppSingleton.getInstance(getApplicationContext()).getRequestQueue().getCache().clear();
+    }
+    */
+
 }
