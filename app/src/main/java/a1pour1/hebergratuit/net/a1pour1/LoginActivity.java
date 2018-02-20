a0a25cf0d66983b73910b8b83a10422908e92def
siiscil <siiscil@hotmail.fr>
2018-02-20 01:07:21 +0100

    Put a Register form and button

--- app/src/main/java/a1pour1/hebergratuit/net/a1pour1/LoginActivity.java
+++ app/src/main/java/a1pour1/hebergratuit/net/a1pour1/LoginActivity.java
@@ -3,6 +3,7 @@ package a1pour1.hebergratuit.net.a1pour1;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -92,6 +93,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+
+    //REGISTER BUTTON BY ANTOINE
+    public void registerButton(View view){
+        startActivity(new Intent(this, activity_register.class));
     }
 
     private void populateAutoComplete() {
---
+++ app/src/main/java/a1pour1/hebergratuit/net/a1pour1/activity_register.java
@@ -0,0 +1,13 @@
+package a1pour1.hebergratuit.net.a1pour1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
