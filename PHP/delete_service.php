4e2ad5bbb25150a9b87479152213763d830df8b0
guillu97 <spore.gui@hotmail.fr>
2018-02-26 13:32:52 +0100

    added delete_service.php I didn't test it

---
+++ PHP/delete_service.php
@@ -0,0 +1,71 @@
+
+<?php
+ 
+/*
+ * Following code will delete a product from table
+ * A product is identified by product id (pid)
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+
+// include db connect class
+require_once "db_connect.php";
+// connecting to db
+$db = new DB();
+$conn = $db->connect();
+
+// to get back later the json object
+echo "JSON:";
+ 
+// check for required fields
+
+
+//TODO: à remettre si GET<->POST
+if (isset($_POST['ServiceId'])) {
+//if (isset($_GET['ProduitId'])) {
+
+    $serviceId = $_POST['ServiceId'];
+	//$pid = $_GET['ProduitId'];
+ 
+
+ 
+
+ 
+    // mysqli delete row with matched pid
+    $sql = "DELETE FROM Service WHERE ServiceId = $serviceId";
+    $result = $conn->query($sql);
+
+ 
+    // check if row deleted or not
+    if ($result) {
+        // successfully updated
+        $response["success"] = 1;
+        $response["message"] = "Product successfully deleted";
+ 
+        // echoing JSON response
+        echo json_encode($response);
+    } else {
+        // no product found
+        $response["success"] = 0;
+        $response["message"] = "No product found";
+ 
+        // echo no users JSON
+        echo json_encode($response);
+    }
+
+
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
+
+// to get back later the json object
+echo ":END";
+
+?>
